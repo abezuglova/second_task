369b9c983d546a1670836dd81700308872547653
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-04-03 12:31:50 +0300

    tasks bloc refactored

--- lib/presentation/pages/tasks_page/bloc/tasks_bloc.dart
+++ lib/presentation/pages/tasks_page/bloc/tasks_bloc.dart
@@ -46,119 +46,121 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
   Future<void> _onTaskAdded(
     _TaskAdded event,
     Emitter<TasksState> emit,
-  ) async {
-    final currentState = state;
-    if (currentState is TasksLoadSuccess) {
-      try {
-        emit(
-          currentState.copyWith(
-            isUpdateInProgress: true,
-          ),
-        );
-        final task = Task(
-          id: -1,
-          name: event.taskName,
-          termDateTime: event.taskTerm,
-          isCompleted: false,
-        );
-        await tasksRepository.addTask(task);
-        _tasksList = await tasksRepository.getTasksList();
-        emit(
-          currentState.copyWith(
-            isUpdateInProgress: false,
-            tasksList: currentState.sortType.sort(_tasksList),
-          ),
-        );
-      } catch (error, stackTrace) {
-        log(
-          'Error during task adding',
-          error: error,
-          stackTrace: stackTrace,
-        );
-        emit(
-          currentState.copyWith(
-            isUpdateInProgress: false,
-            updatingError: error,
-          ),
-        );
-        emit(
-          currentState.copyWith(updatingError: null),
-        );
-      }
-    }
-  }
+  ) async =>
+      await state.mapOrNull<Future>(
+        loadSuccess: (state) async {
+          try {
+            emit(
+              state.copyWith(
+                isUpdateInProgress: true,
+              ),
+            );
+            final task = Task(
+              id: -1,
+              name: event.taskName,
+              termDateTime: event.taskTerm,
+              isCompleted: false,
+            );
+            await tasksRepository.addTask(task);
+            _tasksList = await tasksRepository.getTasksList();
+            emit(
+              state.copyWith(
+                isUpdateInProgress: false,
+                tasksList: state.sortType.sort(_tasksList),
+              ),
+            );
+          } catch (error, stackTrace) {
+            log(
+              'Error during task adding',
+              error: error,
+              stackTrace: stackTrace,
+            );
+            emit(
+              state.copyWith(
+                isUpdateInProgress: false,
+                updatingError: error,
+              ),
+            );
+            emit(
+              state.copyWith(updatingError: null),
+            );
+          }
+        },
+      );
 
   Future<void> _onTaskStatusChanged(
     _TaskStatusChanged event,
     Emitter<TasksState> emit,
-  ) async {
-    final currentState = state;
-    if (currentState is TasksLoadSuccess) {
-      try {
-        emit(
-          currentState.copyWith(
-            isUpdateInProgress: true,
-          ),
-        );
-        await tasksRepository.changeStatus(id: event.id, isCompleted: event.isDone);
-        final taskIndex = _tasksList.indexWhere((task) => task.id == event.id);
-        _tasksList[taskIndex] =
-            _tasksList[taskIndex].copyWith(isCompleted: event.isDone);
-        emit(
-          currentState.copyWith(
-            isUpdateInProgress: false,
-            tasksList: currentState.sortType.sort(_tasksList),
-          ),
-        );
-      } catch (error, stackTrace) {
-        log(
-          'Error during task status changing',
-          error: error,
-          stackTrace: stackTrace,
-        );
-        emit(
-          currentState.copyWith(
-            isUpdateInProgress: false,
-            updatingError: error,
-          ),
-        );
-        emit(
-          currentState.copyWith(updatingError: null),
-        );
-      }
-    }
-  }
+  ) async =>
+      await state.mapOrNull<Future>(
+        loadSuccess: (state) async {
+          try {
+            emit(
+              state.copyWith(
+                isUpdateInProgress: true,
+              ),
+            );
+            await tasksRepository.changeStatus(
+                id: event.id, isCompleted: event.isDone);
+            final taskIndex =
+                _tasksList.indexWhere((task) => task.id == event.id);
+            _tasksList[taskIndex] =
+                _tasksList[taskIndex].copyWith(isCompleted: event.isDone);
+            emit(
+              state.copyWith(
+                isUpdateInProgress: false,
+                tasksList: state.sortType.sort(_tasksList),
+              ),
+            );
+          } catch (error, stackTrace) {
+            log(
+              'Error during task status changing',
+              error: error,
+              stackTrace: stackTrace,
+            );
+            emit(
+              state.copyWith(
+                isUpdateInProgress: false,
+                updatingError: error,
+              ),
+            );
+            emit(
+              state.copyWith(updatingError: null),
+            );
+          }
+        },
+      );
 
   Future<void> _onShowCompletedTasksStatusChanged(
     _ShowCompletedTasksStatusChanged event,
     Emitter<TasksState> emit,
-  ) async {
-    final currentState = state;
-    if (currentState is TasksLoadSuccess) {
-      final tasksList = event.showCompletedTasks
-          ? _tasksList
-          : _tasksList.where((element) => !element.isCompleted).toList();
-      emit(
-        currentState.copyWith(
-          tasksList: currentState.sortType.sort(tasksList),
-          areCompletedTasksShown: event.showCompletedTasks,
-        ),
+  ) async =>
+      await state.mapOrNull<Future>(
+        loadSuccess: (state) async {
+          final tasksList = event.showCompletedTasks
+              ? _tasksList
+              : _tasksList.where((element) => !element.isCompleted).toList();
+          emit(
+            state.copyWith(
+              tasksList: state.sortType.sort(tasksList),
+              areCompletedTasksShown: event.showCompletedTasks,
+            ),
+          );
+        },
       );
-    }
-  }
 
   Future<void> _onTasksSortTypeChanged(
     _TasksSortTypeChanged event,
     Emitter<TasksState> emit,
-  ) async {
-    final currentState = state;
-    if (currentState is TasksLoadSuccess) {
-      emit(
-        currentState.copyWith(
-          tasksList: event.sortType.sort(_tasksList),
-          sortType: event.sortType,
-        ),
+  ) async =>
+      await state.mapOrNull<Future>(
+        loadSuccess: (state) async {
+          emit(
+            state.copyWith(
+              tasksList: event.sortType.sort(_tasksList),
+              sortType: event.sortType,
+            ),
+          );
+        },
       );
-    }
-  }
 }
